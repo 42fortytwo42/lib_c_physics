0b5053f2e02ffa62b2ff135155594b6e4b940a07
42fortytwo42 <tle-mign@student.42.fr>
2014-01-11 18:12:55 +0100

    Create gravitational_third_law.c

---
+++ gravitational_third_law.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gravitational_third_law.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-mign <tle-mign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/10 16:16:32 by tle-mign          #+#    #+#             */
+/*   Updated: 2014/01/10 16:22:09 by tle-mign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	This algorithm implement gravitational capabilities : Newton Third Law
+**
+**	Return : (0) if float force is null.
+**	(float) force is returned. So simple...
+**
+**	Thanks to Universite Caen Basse-Normandie (Biologie) to my physic lessons.
+**	Thomas LE MIGNAN (2014 / Paris - France).
+*/
+
+float	ft_gravitational_third_law(float force)
+{
+	if (force == 0)
+		return (0);
+	return (force);
+}
